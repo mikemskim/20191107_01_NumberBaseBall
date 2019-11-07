41ad765852925b563a869ec8995efa0903e3c4d8
mikemskim <mskim@steconsulting.co.kr>
2019-11-07 20:07:38 +0900

    문제 출제

--- app/src/main/java/com/tj/a20191107_01_numberbaseball/MainActivity.kt
+++ app/src/main/java/com/tj/a20191107_01_numberbaseball/MainActivity.kt
@@ -2,24 +2,48 @@ package com.tj.a20191107_01_numberbaseball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.random.Random
 
 class MainActivity : BaseActivity() {
+
+    var questionNumArray = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createQuestion()
 
         setupEvents()
         setValues()
 
     }
 
+    fun createQuestion() {
+
+
+        while(true) {
+            val number = Random.nextInt(1,9)
+
+            if (!questionNumArray.contains(number)) {
+                questionNumArray.add(number)
+            }
+            if (questionNumArray.size == 3) {
+                break
+            }
+        }
+
+        for (num in questionNumArray) {
+            Log.d("출제숫자",num.toString())
+        }
+
+    }
+
     override fun setupEvents() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun setValues() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 
